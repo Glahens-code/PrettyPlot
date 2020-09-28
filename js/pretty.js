3db3944addf5f581f789702e19436c540b1e3e30
Glahens-code <70273355+Glahens-code@users.noreply.github.com>
2020-09-27 23:30:35 -0400

    final commit

--- js/pretty.js
+++ js/pretty.js
@@ -70,7 +70,21 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
+   var xAxis= d3.axisBottom(xScale);
+
+    var yAxis= d3.axisLeft(yScale);
+    
+    var axes = d3.select("svg")
+        .append("g")
+    axes.append("g")
+        .attr("transform","translate("+margins.left+","
+             +(margins.top+graphDim.height)+")")
+        .call(xAxis)
+    
+    axes.append("g")
+        .attr("transform","translate("+margins.left+","
+             +(margins.top)+")")
+        .call(yAxis)
  
 }
 
@@ -79,6 +93,33 @@ var drawAxes = function(graphDim,margins,
 //margins - objedct that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var labels = d3.select("svg")
+        .append("g")
+        .classed("labels",true)
+        
+    labels.append("text")
+        .text("Trump Support")
+        .classed("title",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graphDim.width/2))
+        .attr("y",margins.top)
+    
+    labels.append("text")
+        .text("Percentage Voting For trump")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graphDim.width/2))
+        .attr("y",(graphDim.height + 35)  + margins.top);
+
+    
+    labels.append("g")
+        .attr("transform","translate(20,"+ 
+              (margins.top+(graphDim.height/2))+")")
+        .append("text")
+        .text("Percent White")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("transform","rotate(90)")
     
 }
 
@@ -97,9 +138,32 @@ var drawLegend = function(graphDim,margins)
            name:"High unemployment"
        }
     ]
+var legend = d3.select("svg")
+        .append("g")
+        .classed("legend",true)
+        .attr("transform","translate("+
+              (margins.left+ 10) +","+
+             (margins.top+10)+")");
+var entries = legend.selectAll("g")
+            .data(categories)
+            .enter()
+            .append("g")
+            .classed("legendEntry", true)
 
+.attr("transform",function(categories,index)
+              {
+                return "translate(0,"+index*20+")";
+              })
+          
 
+entries.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
     
+entries.append("text")
+                .text(function(category){return category.name;})
+                .attr("x",15)
+                .attr("y",10)
     
     
 }
@@ -108,9 +172,9 @@ var drawLegend = function(graphDim,margins)
 var initGraph = function(counties)
 {
     //size of screen
-    var screen = {width:800,height:600}
+    var screen = {width:800,height:650}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:60,right:20,top:30,bottom:50}
     
     
     
@@ -159,6 +223,7 @@ var successFCN = function(counties)
 {
     console.log("politics",counties);
     initGraph(counties);
+  
 }
 
 var failFCN = function(error)
